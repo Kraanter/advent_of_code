8b922aee4d70bd7829b61e90ccffc0da5797bdc3
Krijn Grimme <krijngrimme@Hotmail.com>
2024-12-03 20:42:24 +0100

    Day 2

--- 2024/days/day2.go
+++ 2024/days/day2.go
@@ -1,20 +1,120 @@
 package days
 
-import ()
+import (
+	"adc_2024/util"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 type Day2Solution struct{}
 
 var Day2 Day2Solution = Day2Solution{}
 
 func (d Day2Solution) First(inputLines []string) int {
+	count := 0
+	for _, line := range inputLines {
+		lineCorrect := true
+		heights := strings.Split(line, " ")
 
-	return 1
+		lastValue := 0
+		for i := 0; i < len(heights)-1 && lineCorrect; i++ {
+			leftNum, err := strconv.Atoi(heights[i])
+			if err != nil {
+				panic(1)
+			}
+			rightNum, err := strconv.Atoi(heights[i+1])
+			if err != nil {
+				panic(1)
+			}
+			num := leftNum - rightNum
+			sign := getSign(num)
+			if lastValue == 0 {
+				lastValue = num
+				continue
+			}
+			if sign != getSign(lastValue) || util.AbsoluteInt(num) > 3 || num == 0 {
+				lineCorrect = false
+				break
+			} else {
+				lastValue = num
+			}
+		}
+
+		if lineCorrect {
+			count++
+		}
+	}
+
+	return count
 }
 
 func (d Day2Solution) Second(inputLines []string) int {
-	return 2
+	numberListList := make([][]int, len(inputLines))
+	for lineNum, line := range inputLines {
+		heights := strings.Split(line, " ")
+		numberList := make([]int, len(heights))
+
+		for i, heightStr := range heights {
+			heightNum, err := strconv.Atoi(heightStr)
+			if err != nil {
+				panic(1)
+			}
+
+			numberList[i] = heightNum
+		}
+
+		numberListList[lineNum] = numberList
+	}
+
+	count := 0
+	for _, numberList := range numberListList {
+		if checkSafety(numberList) {
+			count++
+			continue
+		}
+		for i := 0; i < len(numberList); i++ {
+			listToCheck := slices.Delete(slices.Clone(numberList), i, i+1)
+
+			if checkSafety(listToCheck) {
+				count++
+				break
+			}
+		}
+	}
+
+	return count
+}
+
+func checkSafety(numbers []int) bool {
+	lastDiff := 0
+	for i := 0; i < len(numbers)-1; i++ {
+		leftNum := numbers[i]
+		rightNum := numbers[i+1]
+
+		diff := leftNum - rightNum
+		sign := getSign(diff)
+		// println("diff", lastDiff, diff, sign)
+		if lastDiff != 0 && sign != getSign(lastDiff) {
+			return false
+		}
+		if util.AbsoluteInt(diff) > 3 || diff == 0 {
+			return false
+		}
+
+		lastDiff = diff
+	}
+
+	return true
 }
 
 func (d Day2Solution) GetDayNumber() int {
 	return 2
+}
+
+func getSign(num int) int {
+	if num == 0 {
+		return 0
+	}
+	return num / util.AbsoluteInt(num)
 }
